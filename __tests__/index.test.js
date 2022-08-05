1ed440ae0d9da7309df4217ce46cb00f10137f98
Irene De Nicolo <irene.denicolo89@gmail.com>
2022-08-05 11:22:12 +0200

    no relevant change to commit

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,21 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, act } from '@testing-library/react';
 import Home from '../pages/index';
 import Footer from '../components/Footer';
+import ReactDOM from 'react-dom/client';
 import '@testing-library/jest-dom';
 import '../__mocks__/globalFetch';
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
 
 describe('Home component renders correctly', () => {
   it('Shows the first H1 (before "data" loaded) being "Loading..."', () => {
@@ -11,6 +24,27 @@ describe('Home component renders correctly', () => {
       name: 'Loading...',
     })
     expect(heading).toBeDefined()
+    // expect(heading).toBeDefined()
+    act(() => {    
+      document.addEventListener('load',() => {
+        ReactDOM.createRoot(container).render(<Home />);
+        const heading2 = screen.getByRole('heading', {
+          name: 'Latest Posts',
+        })
+        // expect(heading).not.toBeDefined()
+        expect(heading2).toBeDefined()
+      })
+    })
+    
+    // act(() => {
+    //   document.addEventListener('load',() => {
+        // ReactDOM.createRoot(container).render(<Home />);
+        // render(<Home />)
+        
+        // expect(heading).not.toBeDefined()
+      // });
+    // });
+    
   })
 });
 
